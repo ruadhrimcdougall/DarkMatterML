db73c93d850749f3a8c910f33a5d782d8aa4b05f
Ruadhri McDougall <ruadhri.mcdougall@gmail.com>
2024-03-06 19:56:50 +0000

    Adding functions to read in and manipulate the waveform data set (+ the testing file for reference)

---
+++ CNN Analysis/data.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import numpy as np
+
+
+def waveforms_of_truth(waveforms_df, orig_train_df):
+    '''
+    Filter original data set to only include events with runID and eventID present in waveform
+    dataset
+
+    Then reorder the original data set such that the runIDs and the eventIDs line up
+
+    Then save the truth values (i.e. cath-0, gate-1, trit-2) to the waveform dataframe
+    ''' #df1=waveforms_df, df2=orig_train_df
+    # first going to remove any rows in the generated waveform dataset with zero length waveforms
+    try:
+        waveforms_df = waveforms_df.loc[waveforms_df['length'] != 0]
+        print('Rows with length = 0 have been removed from the waveform data set')
+    except KeyError:
+        print("Error: The specified 'length' column does not exist in the DataFrame.")
+    except Exception as e:
+        # Catch other potential exceptions and print the error message
+        print(f"An error occurred: {e}")
+    # waveforms_df.info()
+
+    # Step 1: Filter orig_train_df to only include rows that have matching values in both 'runID' and 'eventID' in waveforms_df.
+    orig_train_df_filtered = orig_train_df.merge(waveforms_df[['runID', 'eventID']], on=['runID', 'eventID'], how='inner')
+
+    # Step 2: Reorder orig_train_df to match the order of waveforms_df.
+    # First, ensure that 'columnA' and 'columnB' are set as indices for both dataframes.
+    waveforms_df = waveforms_df.set_index(['runID', 'eventID'])
+    orig_train_df_filtered = orig_train_df_filtered.set_index(['runID', 'eventID'])
+
+    # Now, reorder df2_filtered to match df1's order. This is done by reindexing df2_filtered based on df1's index.
+    orig_train_df_reordered = orig_train_df_filtered.reindex(waveforms_df.index).reset_index()
+
+    # print(orig_train_df_reordered[['label', 'type']])
+
+    waveforms_df['label'] = orig_train_df_reordered['label'].values
+    waveforms_df['type'] = orig_train_df_reordered['type'].values
+
+    waveforms_df = waveforms_df.reset_index()
+
+    # print(waveforms_df[['label', 'type']])
+
+    return waveforms_df
+
+def pad_waveforms(waveforms_df):
+    '''
+    Expectation is that the waveforms dataframe is put through this function after it has been assigned
+    truth values by the waveforms_of_truth function
+    '''
+    def centre_padding(waveform, max_len):
+        total_padding = max_len - len(waveform)
+        padding_left = total_padding // 2
+        padding_right = total_padding - padding_left
+        return np.pad(waveform, (padding_left, padding_right), mode='constant', constant_values=0)
+    
+    # unsure if I can make the assumption that the times and samples columns are the same length
+    # so adding try/except (could be overkill but oh well)
+    max_length_time = waveforms_df['times'].apply(len).max()
+    max_length_intensity = waveforms_df['samples'].apply(len).max()
+    try:
+        # Try to assert that the maximum lengths are the same
+        assert max_length_time == max_length_intensity
+        # If the assertion passes, set max_length to one of them (since they are equal)
+        max_length = max_length_time
+    except AssertionError:
+        # If the assertion fails, calculate max_length as the max of both
+        max_length = max(max_length_time, max_length_intensity)
+
+    #max_len = waveforms_df['times'].apply(len).max()
+    #waveforms_df['times'] = waveforms_df['times'].apply(lambda x: centre_padding(x, max_length))
+    waveforms_df['padded_samples'] = waveforms_df['samples'].apply(lambda x: centre_padding(x, max_length))#.values()
+    return waveforms_df
---
+++ CNN Analysis/test.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import data
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# read in waveform dataframe
+waveforms = pd.read_parquet('msci_waveforms_df.parquet')
+#waveforms.info()
+#waveforms = waveforms.drop('Unnamed: 0', axis = 1)
+# convert string entries to numpy arrays again (weird issue when saving)
+waveforms['samples'] = waveforms['samples'].apply(np.fromstring)
+waveforms['times'] = waveforms['times'].apply(np.fromstring)
+#waveforms.info()
+# read in tritium and background datasets and combine
+trit_data = pd.read_csv('../Decision Tree Analysis/data/tritium_ML_data.csv')
+bkg_data = pd.read_csv('../Decision Tree Analysis/data/bg_sr1_vetoes_gassplit.csv')
+orig_train = pd.concat([trit_data, bkg_data], ignore_index=True)
+orig_train = orig_train.drop('Unnamed: 0', axis = 1)
+
+print(type(waveforms.at[194, 'times'])) # parquet file keeps it as a numpy array!!!
+# test out the functions
+final_dataset = data.waveforms_of_truth(waveforms, orig_train)
+
+#print(final_dataset.head())
+# final_dataset.info()
+
+# print(final_dataset.runID)
+
+# in the new dataset, runID=6940 eventID=2797 is gas                => check :)
+# in the new dataset, runID=6940 eventID=3177 is cathode            => check :)
+# in the new dataset, runID=6940 eventID=3705 is gate               => check :)
+
+#final_dataset.to_csv('test_final_dataset.csv')
+
+# check the padding works out
+padded_data = data.pad_waveforms(final_dataset)
+padded_data.info()
+
+# padded_data.info()
+rand_event_num = np.random.randint(0, padded_data.shape[0])
+rand_event_samps = padded_data.at[rand_event_num, 'padded_samples']#.to_numpy()
+print(type(rand_event_samps))
+dummy_times = np.linspace(0, len(rand_event_samps)-1, len(rand_event_samps))
+plt.figure()
+plt.plot(dummy_times, rand_event_samps)
+plt.xlabel('Time steps')
+plt.ylabel('Signal Intensity')
+plt.savefig('padding_test.png')
+
+print('\nWaveform Min. Length - ' + str(padded_data['length'].min()))
+print('Waveform Max. Length - ' + str(padded_data['length'].max()))
+print('Waveform Mean Length - ' + str(np.around(padded_data['length'].mean(), 3)))
+print('Waveform Length std. - ' + str(np.around(padded_data['length'].std(), 3)))
